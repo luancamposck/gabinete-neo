4a3a6b36a7632e330ca9fbfa655f11f7c70efb09
LuanCKRG <luancamposck@gmail.com>
2025-09-30 13:37:41 -0300

    feat(/dashboard/mailing-list): add a frontend static to page

--- src/app/dashboard/mailing-list/page.tsx
+++ src/app/dashboard/mailing-list/page.tsx
@@ -1,7 +1,27 @@
+import { Briefcase } from "lucide-react"
+
 const MailingListPage = () => (
-  <div>
-    <h1>Em construção...</h1>
-  </div>
+  <>
+    <div>
+      <h1 className="text-3xl font-semibold mb-4 flex items-center gap-2">
+        <Briefcase className="size-7" /> Mala Direta
+      </h1>
+      <p>
+        Adicione contatos, gerencie endereços e não esqueça nenhum número, tudo
+        isso aqui.
+      </p>
+    </div>
+
+    <div className="flex flex-1 flex-col gap-4">
+      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        <div className="bg-muted/50 aspect-video rounded-xl" />
+        <div className="bg-muted/50 aspect-video rounded-xl" />
+        <div className="bg-muted/50 aspect-video rounded-xl" />
+      </div>
+    </div>
+
+    <div className="bg-muted/50 min-h-[100vh] w-screen flex-1 rounded-xl md:min-h-min"></div>
+  </>
 )
 
 export default MailingListPage
