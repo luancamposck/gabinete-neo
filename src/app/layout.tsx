dc6f5e84345d2aefe8b0443c41997fb6ad41a17b
LuanCKRG <luancamposck@gmail.com>
2025-09-30 07:24:56 -0300

    format(layout): format layout and add to do on readme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
+import { cookies } from "next/headers"
+
+import { AppSidebar } from "@/components/app-sidebar"
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 
 import "./globals.css"
 
@@ -18,13 +22,28 @@ export const metadata: Metadata = {
   description: ""
 }
 
-const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+const RootLayout = async ({
+  children
+}: Readonly<{ children: React.ReactNode }>) => {
+  const cookieStore = await cookies()
+  const defaultOpen = cookieStore.get("sidebar_state")?.value === "true"
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SidebarProvider defaultOpen={defaultOpen}>
+          <AppSidebar />
+
+          <SidebarInset className="overflow-auto">
+            <div className="p-4 lg:p-8">
+              <div className="container mx-auto flex flex-1 flex-col justify-center gap-8">
+                {children}
+              </div>
+            </div>
+          </SidebarInset>
+        </SidebarProvider>
       </body>
     </html>
   )
