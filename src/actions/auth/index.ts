3b30c10f102797f3798a8ea322d3f76223c58bf9
LuanCKRG <luancamposck@gmail.com>
2025-10-22 15:16:53 -0300

    feat(auth): add getSession action for user session retrieval

--- src/actions/auth/index.ts
+++ src/actions/auth/index.ts
@@ -1,4 +1,5 @@
+import getSession from "@/actions/auth/get-session"
 import signIn from "@/actions/auth/sign-in"
 import signUp from "@/actions/auth/sign-up"
 
-export { signUp, signIn }
+export { signUp, signIn, getSession }
