a03d573e230a800e9f7e7e984afcd61320646008
Luan Vitor <luancamposck@gmail.com>
2025-10-23 12:12:10 +0000

    feat(sign-up-schema): add cpf on signup schema

--- src/lib/definitions/sign-up-schema.ts
+++ src/lib/definitions/sign-up-schema.ts
@@ -2,5 +2,9 @@ import { z } from "zod"
 
 export const signUpSchema = z.object({
   email: z.email("Informe um email válido"),
+  cpf: z
+    .string()
+    .transform((val) => val.replace(/\D/g, "")) // remove tudo que não for número
+    .refine((val) => val.length === 11, "CPF deve ter 11 dígitos"),
   password: z.string().min(6, "A senha deve ter pelo menos 6 caracteres")
 })
