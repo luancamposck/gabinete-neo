680c775f17d95d88c54b6521b41402128a5e33a0
LuanCKRG <luancamposck@gmail.com>
2025-10-23 15:22:48 -0300

    feat(sign-up): add email confirmation during user creation

--- src/actions/auth/sign-up.ts
+++ src/actions/auth/sign-up.ts
@@ -65,7 +65,8 @@ async function signUp({
     const { data: authData, error: authError } =
       await supabaseAdmin.auth.admin.createUser({
         email,
-        password
+        password,
+        email_confirm: true
       })
 
     if (authError) {
