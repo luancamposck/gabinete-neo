961c307d72996e34034f0aa3ca7de31932c4e4c6
LuanCKRG <luancamposck@gmail.com>
2025-10-24 11:17:33 -0300

    feat(mailing-list): add data table mailing list to page

--- src/app/dashboard/mailing-list/page.tsx
+++ src/app/dashboard/mailing-list/page.tsx
@@ -1,4 +1,5 @@
 import { Briefcase } from "lucide-react"
+import { MailingListTable } from "@/components/data-tables/mailing-list/mailing-list-table"
 import { MailingListModal } from "@/components/modals/mailing-list-modal"
 
 const MailingListPage = () => (
@@ -16,7 +17,6 @@ const MailingListPage = () => (
         <MailingListModal />
       </div>
     </div>
-
     <div className="flex flex-1 flex-col gap-4">
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
         <div className="bg-muted/50 aspect-video rounded-xl" />
@@ -24,8 +24,8 @@ const MailingListPage = () => (
         <div className="bg-muted/50 aspect-video rounded-xl" />
       </div>
     </div>
-
-    <div className="bg-muted/50 min-h-[100vh] w-screen flex-1 rounded-xl md:min-h-min"></div>
+    {/* <div className="bg-muted/50 min-h-[100vh] w-screen flex-1 rounded-xl md:min-h-min"></div> */}
+    <MailingListTable />
   </>
 )
 
