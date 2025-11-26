3d1cf62b13bee8def6b2fc01e51bc92aef7b3929
LuanCKRG <luancamposck@gmail.com>
2025-11-26 18:59:48 -0300

    feat(service): enhance createOrganizationWithOwnerUserService with error handling and membership creation

--- src/services/create-organization-with-owner-user.service.ts
+++ src/services/create-organization-with-owner-user.service.ts
@@ -1,34 +1,38 @@
 import { deleteAuthUserAdminRepo } from "@/repositories/auth-users/auth-users.admin.repo"
+import { deleteOrganizationsAdminRepo } from "@/repositories/organizations/organizations.admin.repo"
+import { type CreateOrganizationMenbershipServiceParams, createOrganizationMenbershipService } from "@/services/create-organization-menbership.service"
+import { type CreateOrganizationServiceParams, createOrganizationService } from "@/services/create-organizations.service"
 import { type CreateUserServiceParams, createUserWithProfileService } from "@/services/create-user-with-profile.service"
-import { type CreateOrganizationServiceParams, createOrganizationService } from "./create-organizations.service"
+import type { OperationResponse } from "@/types/operation-response"
 
 export interface CreateOrganizationWithOwnerUserServiceParams {
 	user: CreateUserServiceParams
 
-	organization: CreateOrganizationServiceParams
+	organization: Omit<CreateOrganizationServiceParams, "created_by_user_id">
 }
 
-export async function createOrganizationWithOwnerUserService(params: CreateOrganizationWithOwnerUserServiceParams) {
+export async function createOrganizationWithOwnerUserService(params: CreateOrganizationWithOwnerUserServiceParams): Promise<OperationResponse<{ organizationId: string; userId: string }>> {
 	const { user: newUserData, organization: newOrganizationData } = params
 
-	// 1) Criação de um usuário
-	const userWithProfileParams: CreateUserServiceParams = {
-		name: newUserData.name,
-		cpf: newUserData.cpf,
-
+	// 1) Criação de usuário
+	const createUserWithProfileServiceParams: CreateUserServiceParams = {
 		email: newUserData.email,
 		password: newUserData.password,
+
+		name: newUserData.name,
+		cpf: newUserData.cpf,
 		phone: newUserData.phone,
 
 		cep: newUserData.cep,
-		city: newUserData.city,
-		neighborhood: newUserData.neighborhood,
-		number: newUserData.number,
 		street: newUserData.street,
+		number: newUserData.number,
+		neighborhood: newUserData.neighborhood,
+		city: newUserData.city,
 		state: newUserData.state,
 		complement: newUserData.complement
 	}
-	const createUserWithProfileServiceRes = await createUserWithProfileService(userWithProfileParams)
+
+	const createUserWithProfileServiceRes = await createUserWithProfileService(createUserWithProfileServiceParams)
 
 	if (createUserWithProfileServiceRes.success === false) {
 		const errorMessage = createUserWithProfileServiceRes.message
@@ -41,28 +45,65 @@ export async function createOrganizationWithOwnerUserService(params: CreateOrgan
 		}
 	}
 
-	const newUserId = createUserWithProfileServiceRes.data.id
+	const createdUserId = createUserWithProfileServiceRes.data.id
 
-	// 2) Criação de uma Organização
+	// 2) Criação de organização(organization)
 	const createOrganizationServiceParams: CreateOrganizationServiceParams = {
 		name: newOrganizationData.name,
 		slug: newOrganizationData.slug,
-		created_by_user_id: newOrganizationData.created_by_user_id,
-		is_active: newOrganizationData.is_active
+
+		created_by_user_id: createdUserId
 	}
 
 	const createOrganizationServiceRes = await createOrganizationService(createOrganizationServiceParams)
 
 	if (createOrganizationServiceRes.success === false) {
+		await deleteAuthUserAdminRepo({ authUserId: createdUserId })
+
 		const errorMessage = createOrganizationServiceRes.message
 
-		console.log(errorMessage)
-
-		await deleteAuthUserAdminRepo({ authUserId: newUserId })
+		console.error(errorMessage)
 
 		return {
 			success: false,
 			message: errorMessage
+		}
+	}
+
+	const createdOrganizationId = createOrganizationServiceRes.data.organizationId
+
+	// 3) Criar vínculo entre usuário e organização
+	const createOrganizationMenbershipServiceParams: CreateOrganizationMenbershipServiceParams = {
+		organization_id: createdOrganizationId,
+		user_id: createdUserId,
+
+		role: "owner",
+		is_active: true
+	}
+
+	const createOrganizationMenbershipServiceRes = await createOrganizationMenbershipService(createOrganizationMenbershipServiceParams)
+
+	if (createOrganizationMenbershipServiceRes.success === false) {
+		await deleteAuthUserAdminRepo({ authUserId: createdUserId })
+		await deleteOrganizationsAdminRepo({ organizationId: createdOrganizationId })
+
+		const errorMessage = createOrganizationMenbershipServiceRes.message
+
+		console.error(errorMessage)
+
+		return {
+			success: false,
+			message: errorMessage
+		}
+	}
+
+	// 4) Retorno se deu tudo certo
+	return {
+		success: true,
+		message: "Usuário e organização criados com sucesso",
+		data: {
+			organizationId: createdOrganizationId,
+			userId: createdUserId
 		}
 	}
 }
