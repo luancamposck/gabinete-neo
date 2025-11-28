f26d3cfca7cd0f1d9812d4e84555e2a96b6c7825
LuanCKRG <luancamposck@gmail.com>
2025-11-28 06:05:17 -0300

    feat(create-organization): implement action for creating organization with owner user

--- src/actions/create-organization-with-owner-user.action.ts
+++ src/actions/create-organization-with-owner-user.action.ts
@@ -1,6 +1,6 @@
 "use server"
 
-import { createOrganizationWithOwnerSchemaServer } from "@/lib/validations/use-cases/create-organization-with-owner-schema"
+import { createOrganizationWithOwnerSchemaServer } from "@/lib/validations/use-cases/create-organization-with-owner-schema.server"
 import { type CreateOrganizationWithOwnerUserServiceParams, createOrganizationWithOwnerUserService } from "@/services/create-organization-with-owner-user.service"
 import type { OperationResponse } from "@/types/operation-response"
 
