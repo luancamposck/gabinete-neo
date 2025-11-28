b5d5dc2cef7815e454210829aea7caab7a9bc9a2
LuanCKRG <luancamposck@gmail.com>
2025-11-28 06:02:33 -0300

    feat(masks): add mask functions for CEP, CPF, and phone numbers

---
+++ src/lib/masks/index.ts
@@ -0,0 +1,5 @@
+import maskCep from "@/lib/masks/mask-cep"
+import maskCpf from "@/lib/masks/mask-cpf"
+import maskPhone from "@/lib/masks/mask-phone"
+
+export { maskCpf, maskCep, maskPhone }
--- src/lib/utils/masks/index.ts
+++
@@ -1,5 +0,0 @@
-import maskCep from "@/lib/utils/masks/mask-cep"
-import maskCpf from "@/lib/utils/masks/mask-cpf"
-import maskPhone from "@/lib/utils/masks/mask-phone"
-
-export { maskCpf, maskCep, maskPhone }
