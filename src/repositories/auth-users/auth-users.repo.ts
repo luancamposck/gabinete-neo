7312801cf6be63358e1bbbc200a0470db37c1db5
LuanCKRG <luancamposck@gmail.com>
2025-12-02 02:39:05 -0300

    fix(auth): correct import statement formatting and ensure consistent semicolon usage

--- src/repositories/auth-users/auth-users.repo.ts
+++ src/repositories/auth-users/auth-users.repo.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@/lib/supabase/server"
+import { createClient } from "@/lib/supabase/server";
 
 export async function signInAuthUserRepo({ email, password }: { email: string; password: string }) {
 	const supabase = await createClient()
@@ -7,4 +7,10 @@ export async function signInAuthUserRepo({ email, password }: { email: string; p
 		email,
 		password
 	})
+}
+
+export async function getCurrentAuthUserRepo() {
+  const supabase = await createClient()
+
+  return supabase.auth.getUser() // { data: { user }, error }
 }
