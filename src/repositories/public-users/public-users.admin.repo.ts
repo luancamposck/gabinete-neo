05435c79ae9485e8f7b94dc1daa162edf5754360
LuanCKRG <luancamposck@gmail.com>
2025-12-02 02:39:39 -0300

    feat(public-users): add type definition for PublicUserRow and update getPublicUserByUserIdAdminRepo function

--- src/repositories/public-users/public-users.admin.repo.ts
+++ src/repositories/public-users/public-users.admin.repo.ts
@@ -1,9 +1,10 @@
 import type { PostgrestSingleResponse } from "@supabase/supabase-js"
 
-import type { TablesInsert } from "@/lib/definitions/supabase"
+import type { Tables, TablesInsert } from "@/lib/definitions/supabase"
 import { createAdminClient } from "@/lib/supabase/admin"
 
 export type PublicUserInsert = TablesInsert<"users">
+export type PublicUserRow = Tables<"users">
 
 export async function insertPublicUserAdminRepo(insertPublicUserParams: PublicUserInsert): Promise<PostgrestSingleResponse<{ id: string }>> {
 	const supabaseAdmin = createAdminClient()
@@ -15,4 +16,10 @@ export async function deletePublicUserAdminRepo({ publicUserId }: { publicUserId
 	const supabaseAdmin = createAdminClient()
 
 	return supabaseAdmin.from("users").delete().eq("id", publicUserId)
+}
+
+export async function getPublicUserByUserIdAdminRepo({ userId}: {userId: string}): Promise<PostgrestSingleResponse<PublicUserRow>> {
+	const supabaseAdmin = createAdminClient()
+
+	return supabaseAdmin.from("users").select("*").eq("id", userId).single()
 }
