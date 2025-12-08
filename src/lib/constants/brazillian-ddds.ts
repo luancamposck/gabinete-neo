55b40d695997eee7356f2a82f4ddf51d92bf230c
LuanCKRG <luancamposck@gmail.com>
2025-12-08 05:23:27 -0300

    feat(validation): add Brazilian DDDs and update phone validation schema

---
+++ src/lib/constants/brazillian-ddds.ts
@@ -0,0 +1,84 @@
+export const brazilianDdds = [
+	{ ddd: "11", acronym: "SP" },
+	{ ddd: "12", acronym: "SP" },
+	{ ddd: "13", acronym: "SP" },
+	{ ddd: "14", acronym: "SP" },
+	{ ddd: "15", acronym: "SP" },
+	{ ddd: "16", acronym: "SP" },
+	{ ddd: "17", acronym: "SP" },
+	{ ddd: "18", acronym: "SP" },
+	{ ddd: "19", acronym: "SP" },
+
+	{ ddd: "21", acronym: "RJ" },
+	{ ddd: "22", acronym: "RJ" },
+	{ ddd: "24", acronym: "RJ" },
+
+	{ ddd: "27", acronym: "ES" },
+	{ ddd: "28", acronym: "ES" },
+
+	{ ddd: "31", acronym: "MG" },
+	{ ddd: "32", acronym: "MG" },
+	{ ddd: "33", acronym: "MG" },
+	{ ddd: "34", acronym: "MG" },
+	{ ddd: "35", acronym: "MG" },
+	{ ddd: "37", acronym: "MG" },
+	{ ddd: "38", acronym: "MG" },
+
+	{ ddd: "41", acronym: "PR" },
+	{ ddd: "42", acronym: "PR" },
+	{ ddd: "43", acronym: "PR" },
+	{ ddd: "44", acronym: "PR" },
+	{ ddd: "45", acronym: "PR" },
+	{ ddd: "46", acronym: "PR" },
+
+	{ ddd: "47", acronym: "SC" },
+	{ ddd: "48", acronym: "SC" },
+	{ ddd: "49", acronym: "SC" },
+
+	{ ddd: "51", acronym: "RS" },
+	{ ddd: "53", acronym: "RS" },
+	{ ddd: "54", acronym: "RS" },
+	{ ddd: "55", acronym: "RS" },
+
+	{ ddd: "61", acronym: "DF" },
+	{ ddd: "62", acronym: "GO" },
+	{ ddd: "63", acronym: "TO" },
+	{ ddd: "64", acronym: "GO" },
+
+	{ ddd: "65", acronym: "MT" },
+	{ ddd: "66", acronym: "MT" },
+	{ ddd: "67", acronym: "MS" },
+
+	{ ddd: "68", acronym: "AC" },
+	{ ddd: "69", acronym: "RO" },
+
+	{ ddd: "71", acronym: "BA" },
+	{ ddd: "73", acronym: "BA" },
+	{ ddd: "74", acronym: "BA" },
+	{ ddd: "75", acronym: "BA" },
+	{ ddd: "77", acronym: "BA" },
+
+	{ ddd: "79", acronym: "SE" },
+
+	{ ddd: "81", acronym: "PE" },
+	{ ddd: "82", acronym: "AL" },
+	{ ddd: "83", acronym: "PB" },
+	{ ddd: "84", acronym: "RN" },
+	{ ddd: "85", acronym: "CE" },
+	{ ddd: "86", acronym: "PI" },
+	{ ddd: "87", acronym: "PE" },
+	{ ddd: "88", acronym: "CE" },
+	{ ddd: "89", acronym: "PI" },
+
+	{ ddd: "91", acronym: "PA" },
+	{ ddd: "92", acronym: "AM" },
+	{ ddd: "93", acronym: "PA" },
+	{ ddd: "94", acronym: "PA" },
+
+	{ ddd: "95", acronym: "RR" },
+	{ ddd: "96", acronym: "AP" },
+	{ ddd: "97", acronym: "AM" },
+
+	{ ddd: "98", acronym: "MA" },
+	{ ddd: "99", acronym: "MA" }
+]
--- src/lib/validations/users/user-base-schema.client.ts
+++ src/lib/validations/users/user-base-schema.client.ts
@@ -1,12 +1,12 @@
 import { z } from "zod"
 
-import { adressBaseSchemaClient } from "@/lib/validations/atom-schemas/atom-schemas.client"
+import { adressBaseSchemaClient, phoneSchemaClient } from "@/lib/validations/atom-schemas/atom-schemas.client"
 
 export const userBaseSchemaClient = z
 	.object({
 		// Dados do usuário
 		name: z.string().min(3, "Nome do usuário deve ter no mínimo 3 caracteres."),
-		phone: z.string().refine((val) => val.length === 14 || val.length === 15, "Celular inválido. Use (00) 00000-0000 ou (00) 0000-0000"),
+		phone: phoneSchemaClient,
 
 		// Login do usuário
 		email: z.email("Por favor, insira um email válido."),
