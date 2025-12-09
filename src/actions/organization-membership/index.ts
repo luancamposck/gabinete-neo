66f095f6f6c4beeafed2e0097f4df6da04a19dab
LuanCKRG <luancamposck@gmail.com>
2025-12-09 02:18:12 -0300

    feat(organization-memberships): remove listOrganizationMembershipsByOrganizationIdAction from barrel file

--- src/actions/organization-membership/index.ts
+++ src/actions/organization-membership/index.ts
@@ -1,5 +1,4 @@
 import findOrganizationMembershipByUserAndOrganizationAction from "@/actions/organization-membership/find-organization-membership-by-user-and-organization.action"
 import getOrganizationMembershipByUserIdAction from "@/actions/organization-membership/get-organization-membership-by-user-id.action"
-import listOrganizationMembershipsByOrganizationIdAction from "@/actions/organization-membership/list-organization-memberships-by-organization-id.action"
 
-export { findOrganizationMembershipByUserAndOrganizationAction, getOrganizationMembershipByUserIdAction, listOrganizationMembershipsByOrganizationIdAction }
+export { findOrganizationMembershipByUserAndOrganizationAction, getOrganizationMembershipByUserIdAction }
