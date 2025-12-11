9f30df0a3bb01a7aa9a82c1a8048a5b30fd430c5
LuanCKRG <luancamposck@gmail.com>
2025-12-11 06:18:59 -0300

    fix: adjust positioning of background element in login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Vortex } from "@/components/vortex"
 export default function LoginPage() {
 	return (
 		<div className="relative bg-muted/20 flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-			<div className="fixed left-0 -top-40 size-full -z-20 overflow-hidden">
+			<div className="fixed left-0  size-full -z-20 overflow-hidden">
 				<Vortex backgroundColor="transparent" className="flex size-full" rangeY={300} baseRadius={2} particleCount={50} rangeSpeed={1.5} baseHue={200} />
 			</div>
 
