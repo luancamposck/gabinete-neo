47480b331faaba87b60b011e6dbef6d0caacf3c6
LuanCKRG <luancamposck@gmail.com>
2026-01-24 07:28:20 -0300

    feat(theme-provider): set default theme to dark and disable system theme toggle

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -3,5 +3,9 @@
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 
 export const ThemeProvider = ({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) => {
-	return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+	return (
+		<NextThemesProvider defaultTheme="dark" enableSystem={false} {...props}>
+			{children}
+		</NextThemesProvider>
+	)
 }
