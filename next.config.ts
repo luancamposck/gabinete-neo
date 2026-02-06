bb4ac3df9fa2e0e6c57f12367bd62798552d3595
LuanCKRG <luancamposck@gmail.com>
2026-02-06 10:45:07 -0300

    refactor(next.config): remove unnecessary semicolons and improve formatting

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -10,7 +10,12 @@ const nextConfig: NextConfig = {
         pathname: "/storage/v1/object/public/public-assets/**",
       }
     ]
-  }
-};
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+}
 
-export default nextConfig;
+export default nextConfig
