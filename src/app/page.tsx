c4efbe130ca0e20f2f542913d9bb2b0df0ec3feb
luancamposck <luancamposck@gmail.com>
2026-02-12 09:52:21 -0300

    feat(page): implement user redirection to dashboard upon successful login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
+import { redirect } from "next/navigation"
 import { Suspense } from "react"
 
 import { Vortex } from "@/components/vortex"
+import { getCurrentUserAction } from "@/modules/auth/server/slices/get-current-user/actions/get-current-user.action"
 import { AuthTabs, AuthTabsSkeleton } from "@/modules/auth/ui/auth-tabs"
 
 type HomePageProps = {
 	searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }
 
-const HomePage = ({ searchParams }: HomePageProps) => {
+const HomePage = async ({ searchParams }: HomePageProps) => {
+	const getCurrentUserRes = await getCurrentUserAction()
+
+	if (getCurrentUserRes.success) {
+		redirect("/dashboard")
+	}
+
 	return (
 		<div className="relative bg-muted/20 flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
 			<div className="fixed left-0  size-full -z-20 overflow-hidden">
