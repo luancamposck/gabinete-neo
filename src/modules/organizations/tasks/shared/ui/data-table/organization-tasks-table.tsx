72663d3d9af6ea7757cef3c4c7fc2cab9ac60f9c
luancamposck <luancamposck@gmail.com>
2026-03-26 12:55:07 -0700

    feat: [US-019] - Move src/hooks/ to src/shared/hooks/

--- src/modules/organizations/tasks/shared/ui/data-table/organization-tasks-table.tsx
+++ src/modules/organizations/tasks/shared/ui/data-table/organization-tasks-table.tsx
@@ -3,10 +3,10 @@
 import { rankItem } from "@tanstack/match-sorter-utils"
 import { type ColumnDef, type FilterFn, getCoreRowModel, getFacetedRowModel, getFacetedUniqueValues, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, useReactTable } from "@tanstack/react-table"
 import { useState } from "react"
-import { usePersistedTableState } from "@/hooks/use-persisted-table-state"
 import type { OrganizationTaskTableRow } from "@/modules/organizations/tasks/shared/types/organization-tasks-table.types"
 import { DataTable } from "@/shared/components/ui/data-table"
 import { DataTableViewOptions } from "@/shared/components/ui/data-table-view-options"
+import { usePersistedTableState } from "@/shared/hooks/use-persisted-table-state"
 
 import { organizationTasksColumns } from "./columns"
 import { OrganizationTasksTableToolbar } from "./organization-tasks-table-toolbar"
